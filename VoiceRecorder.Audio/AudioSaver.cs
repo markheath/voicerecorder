ed37582e08e25e8ee558806ba0487881f487a801
markheath <markheath@localhost>
2010-10-06 06:32:00 +0000

    refactoring autotune conversion into new file

--- VoiceRecorder.Audio/AudioSaver.cs
+++ VoiceRecorder.Audio/AudioSaver.cs
@@ -53,22 +53,7 @@ namespace VoiceRecorder.Audio
             {
                 string tempFile = WavFileUtils.GetTempWavFileName();
                 tempFiles.Add(tempFile);
-                using (WaveFileReader reader = new WaveFileReader(fileToProcess))
-                {
-                    IWaveProvider stream32 = new Wave16toIeeeProvider(reader);
-                    IWaveProvider streamEffect = new AutoTuneWaveProvider(stream32);
-                    IWaveProvider stream16 = new WaveIeeeTo16Provider(streamEffect);
-                    using (WaveFileWriter converted = new WaveFileWriter(tempFile, stream16.WaveFormat))
-                    {
-                        byte[] buffer = new byte[2048 * 4];
-                        int bytesRead;
-                        do
-                        {
-                            bytesRead = stream16.Read(buffer, 0, buffer.Length);
-                            converted.WriteData(buffer, 0, bytesRead);
-                        } while (bytesRead != 0 && converted.Length < reader.Length);
-                    }
-                }
+                AutoTuneUtils.ApplyAutotune(fileToProcess, tempFile);
                 fileToProcess = tempFile;
             }
             if (SaveFileFormat == SaveFileFormat.Mp3)
