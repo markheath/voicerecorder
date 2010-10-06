24b5f5aaa90fcd5e9dffa9c36806f829d15caf69
Mark Heath <147668+markheath@users.noreply.github.com>
2010-10-06 06:32:00 +0000

    refactoring autotune conversion into new file

---
+++ VoiceRecorder.Audio/AutoTuneUtils.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using NAudio.Wave;
+
+namespace VoiceRecorder.Audio
+{
+    public class AutoTuneUtils
+    {
+        public static void ApplyAutotune(string fileToProcess, string tempFile)
+        {
+            using (WaveFileReader reader = new WaveFileReader(fileToProcess))
+            {
+                IWaveProvider stream32 = new Wave16toIeeeProvider(reader);
+                IWaveProvider streamEffect = new AutoTuneWaveProvider(stream32);
+                IWaveProvider stream16 = new WaveIeeeTo16Provider(streamEffect);
+                using (WaveFileWriter converted = new WaveFileWriter(tempFile, stream16.WaveFormat))
+                {
+                    byte[] buffer = new byte[2048 * 4];
+                    int bytesRead;
+                    do
+                    {
+                        bytesRead = stream16.Read(buffer, 0, buffer.Length);
+                        converted.WriteData(buffer, 0, bytesRead);
+                    } while (bytesRead != 0 && converted.Length < reader.Length);
+                }
+            }
+        }
+    }
+}
