d9aa1227ca4f2f2bc30870e93a61303121f699f8
markheath <markheath@localhost>
2010-10-07 21:57:26 +0000

    added in some performance testing stuff

---
+++ VoiceRecorder.Tests/PerformanceTests.cs
@@ -0,0 +1,84 @@
+using NUnit.Framework;
+using NAudio.Wave;
+using VoiceRecorder.Audio;
+using System;
+using System.Diagnostics;
+
+namespace VoiceRecorder.Tests 
+{
+    [TestFixture]
+    public class PerformanceTests 
+    {
+        [Test]
+        public void TimingTest() 
+        {
+            Stopwatch timer = new Stopwatch();
+            int iterations = 10;
+            long ms = timer.Time(() => ReadFromTestProvider(1024 * 1024), iterations);
+            Console.WriteLine("{0} ms", ms);
+        }
+
+        private static void ReadFromTestProvider(int bytesToRead)
+        {
+            TestWaveProvider source = new TestWaveProvider(44100, 1);
+            AutoTuneWaveProvider autoTune = new AutoTuneWaveProvider(source);
+            byte[] buffer = new byte[1024 * 4];
+            int bytesRead = 0;
+            while (bytesRead < bytesToRead)
+            {
+                bytesRead += autoTune.Read(buffer, 0, buffer.Length);
+            }
+        }
+
+
+    }
+
+    // http://stackoverflow.com/questions/232848/wrapping-stopwatch-timing-with-a-delegate-or-lambda
+    static class StopwatchExtensions
+    {
+        public static long Time(this Stopwatch sw, Action action, int iterations)
+        {
+            sw.Reset();
+            sw.Start();
+            for (int i = 0; i < iterations; i++)
+            {
+                action();
+            }
+            sw.Stop();
+
+            return sw.ElapsedMilliseconds;
+        }
+    }
+
+    class TestWaveProvider : WaveProvider32
+    {
+        float[] testData;
+        int testIndex;
+
+        public TestWaveProvider(int sampleRate, int channels) : base(sampleRate, channels)
+        {
+            testData = new float[sampleRate * channels * 4]; // four seconds of audio      
+            // for now, our test data is a sine wave
+            float Frequency = 517;
+            float Amplitude = 0.25f;
+            for (int sample = 0; sample < testData.Length; sample++)
+            {
+                testData[sample] = (float)(Amplitude * Math.Sin((2 * Math.PI * sample * Frequency) / sampleRate));
+            }
+        }
+                
+        public override int Read(float[] buffer, int offset, int sampleCount)
+        {
+
+            for (int n = 0; n < sampleCount; n++)
+            {
+                buffer[offset + n] = testData[testIndex++];
+                if (testIndex >= testData.Length)
+                {
+                    testIndex = 0;
+                }
+            }
+            return sampleCount;
+        }
+    }
+}
